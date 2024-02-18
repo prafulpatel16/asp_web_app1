57af7b94bd8597c02bfc421f0a6f8cef637a24af
prafulpatel16 <praful1611@outlook.com>
2024-02-18 14:07:57 -0600

    add new text file

--- obj/Debug/netcoreapp3.1/praful_cloud_app.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/praful_cloud_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d7bc6fb371e096af2e1e6bcc20ac12cc4351d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723fc2d6c10736f591717e659720b4025da4586c")]
 [assembly: System.Reflection.AssemblyProductAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
