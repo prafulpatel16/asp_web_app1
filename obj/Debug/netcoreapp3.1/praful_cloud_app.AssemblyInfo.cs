21d7bc6fb371e096af2e1e6bcc20ac12cc4351d9
prafulpatel16 <praful1611@outlook.com>
2024-02-18 13:17:59 -0600

    web app code added

--- obj/Debug/netcoreapp3.1/praful_cloud_app.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/praful_cloud_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a21dd9c96e53b10e73ec8ccd35b689cacabd2ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("praful_cloud_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
